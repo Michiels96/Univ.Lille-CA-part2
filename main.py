a0f0e095579d0ab451769c1797e8860eb4abda69
Thierno Souleymane BAH <thierno-souleymane.bah@leroymerlin.fr>
2021-10-18 04:36:09 +0200

    feat(essentials features done)

--- main.py
+++ main.py
@@ -18,6 +18,32 @@ def process(cmd):
         print(e)
         return False, ""
 
+def verify_signature(user, signature, message):
+    """
+    """
+    # Convertir le certificat en binaire
+    with open("tmp.sign", "wb") as f:
+        f.write(signature.encode("latin-1"))
+    
+    # Convertir le certificat en binaire
+    with open("from.tmp", "w") as f:
+        f.write(message)
+
+    # Vérification du certificat du client
+    ok, _ = process(
+        f"(openssl dgst -sha1 -verify {ROOT_DIR}/{user}/{user}.pub -signature tmp.sign from.tmp)")
+
+    _, _ = process("rm from.tmp tmp.sign")
+
+    return ok
+
+def arrange_paths(paths, user):
+    """
+    """
+    print(paths) # F E D => F G E D
+    
+    return  [[path[-1]] + [user] + path[:-1] for path in paths]
+
 
 def create_keys(user):
     """
@@ -30,7 +56,7 @@ def create_keys(user):
     return ok
 
 
-def init():
+def init(user1, user2):
     """
     """
     ok, _ = process(f"mkdir {ROOT_DIR}")
@@ -38,12 +64,12 @@ def init():
         return False
 
     # Create root user and two init users
-    ok = create_keys("root") and add_user("A", "root") and add_user("B", "root")
+    ok = create_keys("root") and add_user(user1, "root") and add_user(user2, "root")
     if not ok:
         return False
 
     # Link the two previous users
-    ok = exchange_pubkeys("A", "B", is_direct=True)
+    ok = exchange_pubkeys(user1, user2, is_direct=True)
     if not ok:
         return False
 
@@ -77,8 +103,8 @@ def all_paths(src, dst, explored):
     else:
         for friend in [file.split(".")[0] for file in os.listdir(f"{ROOT_DIR}/{src}/friends") if file not in explored]:
             for path in all_paths(friend, dst, explored):
-                # path.insert(0, friend)
-                path.append(friend)
+                path.insert(0, friend)
+                # path.append(friend)
                 paths.append(path)
     return paths
 
@@ -89,23 +115,6 @@ def best_path(paths):
     paths.sort()
     return paths[0]
 
-def sign_message_by_user(user, message):
-    """
-    """
-    ok, _ = process(
-        f"openssl dgst -sha1 -sign {ROOT_DIR}/{user}/{user}.rsa -binary message | openssl enc -base64")
-    if not ok:
-        return ""
-
-    # Parsing du certificat en un format transportable
-    with open("signature.sign", "rb") as f:
-        signature = f.read()
-
-    ok, _ = process(f"rm signature.sign")
-
-    return signature
-
-
 
 def send_pubkey_by_path(path):
     """
@@ -114,37 +123,93 @@ def send_pubkey_by_path(path):
     for i, user in enumerate(path):
         if i == 0:
             # Sign the pub key
-            signature = sign_message_by_user(user, {ROOT_DIR}/{path[-2]}/{path[-2]}.pub)
+            ok, _ = process(
+                f"openssl dgst -sha1 -sign {ROOT_DIR}/{user}/{user}.rsa -out pubkey.sign {ROOT_DIR}/{path[-2]}/{path[-2]}.pub ")
+            if not ok:
+                return True
+            
+            # Parsing du certificat en un format transportable
+            with open("pubkey.sign", "rb") as f:
+                signature = f.read()
 
+            ok, _ = process(f"rm pubkey.sign")
+
+    
             # Crypt pubkey
-            ok, signed_pubkey = process(f"openssl rsautl -encrypt -pubin -inkey {ROOT_DIR}/{user}/{path[i+1]}.pub -in {ROOT_DIR}/{user}/{path[-1]}.pub | openssl enc -base64")
+            # print(f"{ROOT_DIR}/{user}/friends/{path[i+1]}.pub", f"{ROOT_DIR}/{user}/friends/{path[-1]}.pub")
+            # print(f"openssl rsautl -encrypt -pubin -inkey {ROOT_DIR}/{user}/friends/{path[i+1]}.pub -in {ROOT_DIR}/{user}/friends/{path[-1]}.pub | openssl enc -base64")
+            # ok, signed_pubkey = process(f"openssl -encrypt -pubin -inkey {ROOT_DIR}/{user}/friends/{path[i+1]}.pub -in {ROOT_DIR}/{user}/friends/{path[-1]}.pub | openssl enc -base64")
+            ok, pubkey = process(f"cat {ROOT_DIR}/{user}/friends/{path[-1]}.pub")
             if not ok:
                 return ok
 
             # Send the request to the next user
             with open("answer.json", "w") as f:
                 f.write(json.dumps({
-                    "signature": signature,
-                    "signed_pubkey": signed_pubkey
+                    "signature": signature.decode("latin-1"),
+                    "signed_pubkey": "\n" + pubkey
                 }))
             ok, _ = process(f"mv answer.json {ROOT_DIR}/{path[i+1]}/")
             if not ok:
                 return ok
+
         elif i == len(path) - 2:
-            # Decrypt and store pubkey
+            with open(f"{ROOT_DIR}/{user}/answer.json", "r") as f:
+                answer = json.load(f)
+
+            # Store incoming pubkey
+            pubkey = answer["signed_pubkey"] # Should be decrypted
+            ok, _ = process(
+                f"cat > {ROOT_DIR}/{user}/friends/{path[-1]}.pub << EOF && echo -e {pubkey}")
+            if not ok:
+                return ok
+
             # Crypt his pubkey
+            ok, pubkey = process(f"cat {ROOT_DIR}/{user}/{user}.pub") # Should be encryted
+            if not ok:
+                return ok
+            answer["signed_pubkey"] = "\n" + pubkey
+
             # Send the request (pubkey + signature) to the next user
-            return True
+            with open(f"{ROOT_DIR}/{user}/answer.json", "w") as f:
+                    f.write(json.dumps(answer))
+            ok, _ = process(f"mv {ROOT_DIR}/{user}/answer.json {ROOT_DIR}/{path[i+1]}/")
+
+            if not ok:
+                return ok
+        
         elif i == len(path) - 1:
-            # Decrypt and store pubkey
-            # Verify signature
-            # Send the request (pubkey + signature) to the next user
-            return True
-        else:
+            with open(f"{ROOT_DIR}/{user}/answer.json", "r") as f:
+                answer = json.load(f)
             # Decrypt pubkey
-            # Crypt pubkey
+            pubkey = answer["signed_pubkey"] # Should be decrypt
+
+            # Verify signature
+            if not verify_signature(path[0], answer["signature"], pubkey[1:]):
+                return False
+
+            # Store pubkey
+            ok, _ = process(
+                f"cat > {ROOT_DIR}/{user}/friends/{path[-2]}.pub << EOF && echo -e {pubkey}")
+            if not ok:
+                return ok
+    
+        else:
+            with open(f"{ROOT_DIR}/{user}/answer.json", "r") as f:
+                answer = json.load(f)
+            # Decrypt pubkey
+            pubkey = answer["signed_pubkey"] # Should be decrypt
+
+            # Crypt pubkey again
+            answer["signed_pubkey"] = "\n" + pubkey
+
             # Send the request to the next user
-            return True 
+            with open(f"{ROOT_DIR}/{user}/answer.json", "w") as f:
+                    f.write(json.dumps(answer))
+            ok, _ = process(f"mv {ROOT_DIR}/{user}/answer.json {ROOT_DIR}/{path[i+1]}/")
+            if not ok:
+                return ok
+
     return True
 
 def exchange_pubkeys(requester, requested, is_direct=False):
@@ -156,7 +221,11 @@ def exchange_pubkeys(requester, requested, is_direct=False):
         if not ok:
             err_msg = "+ Clefs publique non échangées"
     else:
-        paths = all_paths(requested, requester, ["root.pub"])
+        paths = arrange_paths(all_paths(requested, requester, ["root.pub"]), requested)
+        print("+ The paths are:")
+        for p in paths:
+            print(f"\t+ {p}")
+
         if len(paths) == 0:
             return False, "+ Aucuns amis en commun"
 
@@ -166,18 +235,21 @@ def exchange_pubkeys(requester, requested, is_direct=False):
             return False, ""
 
         path = best_path(paths)
-        print(f"+ Best path: {path}")
-        send_pubkey_by_path([path[0]] + [requested] + path[1:])
+        print(f"\n+ Best path: {path}")
+        ok = send_pubkey_by_path(path)
 
 
-    return True, err_msg
+    return ok, err_msg
 
 
 
 def main():
-    init()
-    add_user("C", "B")
-    exchange_pubkeys("C", "A")
+    init("D", "E")
+    add_user("F", "E")
+    add_user("G", "D")
+    exchange_pubkeys("F", "G")
+    add_user("H", "D")
+    exchange_pubkeys("H", "E")
 
 
 if __name__ == "__main__":
